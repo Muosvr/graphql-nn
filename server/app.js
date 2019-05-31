76bf986ed43ad6427d8b7013b1fbcfebbdb92a6b
Muosvr <lujason2015@gmail.com>
2019-05-30 23:36:41 -0400

    2:32:08

--- server/app.js
+++ server/app.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
+
+
+mongoose.connect('mongodb+srv://jason:jason123@cluster0-xsswl.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true })
+  .then(() => console.log('MongoDB connected'))
 
 const app = express();
 const port = process.env.PORT || 4000
